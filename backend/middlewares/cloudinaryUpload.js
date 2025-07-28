a60ddd50f61619ce1b63fb41c24c3e0567fd5edb
nupshenoy <nupurshenoy@gmail.com>
2025-07-28 12:12:30 +0530

    cloud fixed

--- backend/middlewares/cloudinaryUpload.js
+++ backend/middlewares/cloudinaryUpload.js
@@ -1,11 +1,14 @@
 import multer from 'multer';
 import { v2 as cloudinary } from 'cloudinary';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import dotenv from "dotenv";
+
+dotenv.config();
 
 cloudinary.config({
-  cloud_name: 'dsdiiuvnl',
-  api_key: '319521699458375',
-  api_secret: 'AzNgId0H1gmUTZyBl4oCSbmQPh4',
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
